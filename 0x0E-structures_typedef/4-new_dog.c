0a764df4f94f150b234b6215c63adf8902ed8ad1
Emmanuela-ikpontu <ikpontuemmanuela@gmail.com>
2022-05-12 07:41:21 -0700

    4-new_dog.c

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -14,21 +14,18 @@ char *s;
 
 if (str == NULL)
 return (0);
-
 l = 0;
 while (*(str + l))
 l++;
-
 s = malloc(sizeof(char) * l + 1);
-
 if (s == 0)
 return (0);
-
 for (i = 0; i <= l; i++)
 {
 *(s + i) = *(str + i);
 }
 return (s);
+}
 /**
  * new_dog - creates a new dog
  * @name: name of dog
@@ -41,12 +38,9 @@ return (s);
 dog_t *new_dog(char *name, float age, char *owner)
 {
 dog_t *new_dog;
-
 new_dog = malloc(sizeof(struct dog));
-
 if (new_dog == 0 || name == 0 || owner == 0)
 return (0);
-
 new_dog->name = _strdup(name);
 if (new_dog->name == 0)
 {
